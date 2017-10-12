0c73270180d626850df75f7c584498265e01ad81
Muhammad Waseem <theprowaseem@gmail.com>
2017-10-12 19:54:37 +0500

    updating server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
 });
 
 
-const port = process.env.port || '3000';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 const server = http.createServer(app);
